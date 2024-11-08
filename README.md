c3f6c46b5ddc73bb4c8c03057b67cba2dc66332b
Wagner Rodrigues <wagnerlsr.ti@gmail.com>
2024-11-08 14:58:56 -0300

    documentação

---
+++ README.md
@@ -0,0 +1,50 @@
+# Sirio Libânes
+
+## Documentação
+
+Github:
+
+* [Clonar o projeto do Github](https://github.com/wagnerlsr/sirio-libanes)
+
+###
+## Instruções para build e execução do projeto
+
+Para buildar o aplicatico acessar o diretorio do projeto e executar o comando abaixo: 
+* `mvn clean install`
+
+Após o build entrar na pasta target/ e executar a aplicação:
+* `java -jar sirio-0.0.1-SNAPSHOT.jar`
+
+Para executar os testes unitários:
+* `mvn test`
+
+###
+## Acesso a aplicação e execução dos testes
+
+Para acessar o cliente da API usar o link abaixo:
+* [Link de acesso ao portal](http://localhost:3000/swagger-ui/index.html)
+* [POST /login](http://localhost:3000/swagger-ui/index.html#/user-controller/authenticateUser) - endpoint de login para recuparação do token de acesso
+
+## Endpoints com a role ADMIN para acesso por usuários com perfil de administrador:
+* [POST /signup](http://localhost:3000/swagger-ui/index.html#/user-controller/createUser) - endpoint para criação de um usuário
+* [GET /users](http://localhost:3000/swagger-ui/index.html#/user-controller/getUsers) - endpoint para listar todos usuários
+* [DELETE /users/{cpf}](http://localhost:3000/swagger-ui/index.html#/user-controller/removeUser) - endpoint para criação de um usuário
+
+## Endpoints com role USER para acesso dos usuários normais:
+Um usuário só pode acessar e alterar os próprios dados
+* [GET /users/{cpf}](http://localhost:3000/swagger-ui/index.html#/user-controller/removeUser) - endpoint para listar um usuário
+* [PATCH /users/{cpf}](http://localhost:3000/swagger-ui/index.html#/user-controller/updateUser) - endpoint para atualizar dados do usuário
+
+##
+## Informações gerais
+Quando aplicação é executada são criados três registros no banco de dados para manutenção dos sistema:
+####
+Serão criadas duas Roles
+* `ADMIN - para usuários com perfil de administrador`
+* `USER - para usuários normais`
+
+Será criado um usário com perfil administrador
+* `Name: Admin`
+* `CPF: 99999999999`
+* `Password: admin`
+
--- src/test/java/br/com/sirio/auth/services/UserAuthServiceImplTests.java
+++ src/test/java/br/com/sirio/auth/services/UserAuthServiceImplTests.java
@@ -1,165 +1,121 @@
-//package br.com.sirio.auth.services;
-//
-//import br.com.sirio.SirioApplication;
-//import br.com.sirio.dtos.UpdateUserDto;
-//import br.com.sirio.models.Role;
-//import br.com.sirio.models.StatusType;
-//import br.com.sirio.models.User;
-//import br.com.sirio.repositories.UserRepository;
-//import br.com.sirio.services.UserService;
-//import br.com.sirio.services.impl.UserServiceImpl;
-//import com.fasterxml.jackson.databind.JsonMappingException;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import java.time.Instant;
-//import java.util.Arrays;
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.ArgumentMatchers.eq;
-//import static org.mockito.Mockito.when;
-//
-//
-//@ActiveProfiles("test")
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = SirioApplication.class)
-//class UserAuthServiceImplTests {
-//
-//    @MockBean(name="userRepository")
-//    UserRepository userRepository;
-//    UserService userService;
-//    Role roleAdmin;
-//    Role roleUser;
-//
-//    List<User> users;
-//    List<UpdateUserDto> usersDto;
-//
-//    @BeforeEach
-//    public void setUp() {
-//        roleAdmin = Role.builder().id(1L).name(Role.RoleName.ADMIN).build();
-//        roleUser = Role.builder().id(2L).name(Role.RoleName.USER).build();
-//
-//        users = Arrays.asList(
-//                User.builder()
-//                        .id(1L)
-//                        .name("Admin")
-//                        .cpf("11111111111")
-//                        .password("11111")
-//                        .createdAt(Instant.now())
-//                        .status(StatusType.ACTIVE)
-//                        .roles(List.of(roleAdmin))
-//                        .build(),
-//                User.builder()
-//                        .id(2L)
-//                        .name("Usuario 2")
-//                        .cpf("22222222222")
-//                        .password("22222")
-//                        .zipCode(2)
-//                        .address("Rua 2")
-//                        .numberAddress(2)
-//                        .additionalAddress("Complemento 2")
-//                        .district("Cidade 2")
-//                        .state("SP")
-//                        .createdAt(Instant.now())
-//                        .status(StatusType.ACTIVE)
-//                        .roles(List.of(roleUser))
-//                        .build(),
-//                User.builder()
-//                        .id(1L)
-//                        .name("Usuario 3")
-//                        .cpf("33333333333")
-//                        .password("33333")
-//                        .createdAt(Instant.now())
-//                        .status(StatusType.ACTIVE)
-//                        .roles(List.of(roleUser))
-//                        .build()
-//        );
-//
-//        usersDto = Arrays.asList(
-//                UpdateUserDto.builder()
-//                        .name("Usuario 1")
-//                        .zipCode(1)
-//                        .address("Rua 1")
-//                        .numberAddress(1)
-//                        .additionalAddress("Complemento 1")
-//                        .district("Cidade 1")
-//                        .state("SP")
-//                        .build(),
-//                UpdateUserDto.builder()
-//                        .name("Usuario 2")
-//                        .zipCode(2)
-//                        .address("Rua 2")
-//                        .numberAddress(2)
-//                        .additionalAddress("Complemento 2")
-//                        .district("Cidade 2")
-//                        .state("SP")
-//                        .build(),
-//                UpdateUserDto.builder()
-//                        .name("Usuario 3")
-//                        .zipCode(3)
-//                        .address("Rua 3")
-//                        .numberAddress(3)
-//                        .additionalAddress("Complemento 3")
-//                        .district("Cidade 3")
-//                        .state("SP")
-//                        .build()
-//        );
-//
-//        userService = new UserServiceImpl(userRepository);
-//    }
-//
-//    @Test
-//    public void getUsers_Success() {
-//        when(userRepository.findAll()).thenReturn(users);
-//
-//        List<User> result = userService.getUsers();
-//
-//        assertThat( result.size() ).isEqualTo(3);
-//        assertThat( result.containsAll(users) ).isTrue();
-//    }
-//
-//    @Test
-//    public void removeUser_Success() {
-//        when(userRepository.findByCpf(eq(users.get(1).getCpf()))).thenReturn(Optional.of(users.get(1)));
-//        when(userRepository.save(any())).thenReturn( users.get(1) );
-//
-//        User result = userService.removeUser(users.get(1).getCpf(), users.get(1).getId());
-//
-//        assertThat(result).isNotNull();
-//        assertThat(result).isInstanceOf(User.class);
-//        assertThat(result.getDeletedAt()).isNotNull();
-//    }
-//
-//    @Test
-//    public void updateUser_Success() throws JsonMappingException {
-//        when(userRepository.findByCpf(eq(users.get(1).getCpf()))).thenReturn(Optional.of(users.get(1)));
-//        when(userRepository.save(any())).thenReturn( users.get(1) );
-//
-//        User result = userService.updateUser(users.get(1).getCpf(), usersDto.get(1), 1L);
-//
-//        assertThat(result).isNotNull();
-//        assertThat(result).isInstanceOf(User.class);
-//        assertThat(result.getAdditionalAddress()).isEqualTo(users.get(1).getAdditionalAddress());
-//    }
-//
-//    @Test
-//    public void updateUser_ExceptionError() throws JsonMappingException {
-//        when(userRepository.findByCpf(eq(users.get(1).getCpf()))).thenThrow(new RuntimeException("Usuário não localizado."));
-//
-//        Exception exception = assertThrows(Exception.class, () -> {
-//            userService.updateUser(users.get(1).getCpf(), usersDto.get(1), 1L);
-//        });
-//
-//        assertEquals("Usuário não localizado.", exception.getMessage());
-//    }
-//}
+package br.com.sirio.auth.services;
+
+import br.com.sirio.SirioApplication;
+import br.com.sirio.dtos.UpdateUserDto;
+import br.com.sirio.models.Role;
+import br.com.sirio.models.StatusType;
+import br.com.sirio.models.User;
+import br.com.sirio.repositories.UserRepository;
+import br.com.sirio.services.UserService;
+import br.com.sirio.services.impl.UserServiceImpl;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SirioApplication.class)
+class UserAuthServiceImplTests {
+
+    @MockBean(name="userRepository")
+    UserRepository userRepository;
+    UserService userService;
+    Role roleAdmin;
+    Role roleUser;
+
+    List<User> users;
+    List<UpdateUserDto> usersDto;
+
+    @BeforeEach
+    public void setUp() {
+        roleAdmin = Role.builder().id(1L).name(Role.RoleName.ADMIN).build();
+        roleUser = Role.builder().id(2L).name(Role.RoleName.USER).build();
+
+        users = Arrays.asList(
+                User.builder()
+                        .id(1L)
+                        .name("Admin")
+                        .cpf("11111111111")
+                        .password("11111")
+                        .createdAt(Instant.now())
+                        .status(StatusType.ACTIVE)
+                        .roles(List.of(roleAdmin))
+                        .build(),
+                User.builder()
+                        .id(2L)
+                        .name("Usuario 2")
+                        .cpf("22222222222")
+                        .password("22222")
+                        .zipCode(2)
+                        .address("Rua 2")
+                        .numberAddress(2)
+                        .additionalAddress("Complemento 2")
+                        .district("Cidade 2")
+                        .state("SP")
+                        .createdAt(Instant.now())
+                        .status(StatusType.ACTIVE)
+                        .roles(List.of(roleUser))
+                        .build(),
+                User.builder()
+                        .id(1L)
+                        .name("Usuario 3")
+                        .cpf("33333333333")
+                        .password("33333")
+                        .createdAt(Instant.now())
+                        .status(StatusType.ACTIVE)
+                        .roles(List.of(roleUser))
+                        .build()
+        );
+
+        usersDto = Arrays.asList(
+                UpdateUserDto.builder()
+                        .name("Usuario 1")
+                        .zipCode(1)
+                        .address("Rua 1")
+                        .numberAddress(1)
+                        .additionalAddress("Complemento 1")
+                        .district("Cidade 1")
+                        .state("SP")
+                        .build(),
+                UpdateUserDto.builder()
+                        .name("Usuario 2")
+                        .zipCode(2)
+                        .address("Rua 2")
+                        .numberAddress(2)
+                        .additionalAddress("Complemento 2")
+                        .district("Cidade 2")
+                        .state("SP")
+                        .build(),
+                UpdateUserDto.builder()
+                        .name("Usuario 3")
+                        .zipCode(3)
+                        .address("Rua 3")
+                        .numberAddress(3)
+                        .additionalAddress("Complemento 3")
+                        .district("Cidade 3")
+                        .state("SP")
+                        .build()
+        );
+
+        userService = new UserServiceImpl(userRepository);
+    }
+
+}
